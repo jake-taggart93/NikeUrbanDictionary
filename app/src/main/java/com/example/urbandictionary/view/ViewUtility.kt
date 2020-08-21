1d0f82df39afa250356d32abd6e1ce78dbae1aaf
Jake <taggart.jacob93@gmail.com>
2020-08-21 03:28:44 -0400

    Specify type explicitly for extension function

--- app/src/main/java/com/example/urbandictionary/view/ViewUtility.kt
+++ app/src/main/java/com/example/urbandictionary/view/ViewUtility.kt
@@ -2,10 +2,11 @@ package com.example.urbandictionary.view
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 
 fun Context.makeLongToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 
-fun ViewGroup.inflate(@LayoutRes layout: Int) = LayoutInflater.from(context).inflate(layout, this, false)
+fun ViewGroup.inflate(@LayoutRes layout: Int): View = LayoutInflater.from(context).inflate(layout, this, false)
